eaf90c3623b10a702c07513351725340fe5fb4c6
Koshikawaxxx1927 <ebi1927@icloud.com>
2023-02-07 23:19:49 +0900

    1st version finished but some functions for a useful timer is lacking.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const $btn = document.querySelector(".btn");
     const $minutes = document.querySelector(".minutes");
     const $seconds = document.querySelector(".seconds");
+    const $counter = document.querySelector(".timer-title");
     let timer;
+    let st;
     $btn.addEventListener("click", () => {
         timer = new SendToTimer($minutes, $seconds, $btn);
-        timer.color1 = "red";
+        // timer.color1 = "red";
         timer.getTimer();
     });
 });
